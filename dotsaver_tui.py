bfe2a30f0755b7cb17f7a0508f68f7ee61cb04a0
Christoph Hehl <chehl@marco.de>
2024-11-29 18:27:26 +0100

    base structure created

--- dotsaver_tui.py
+++ dotsaver_tui.py
@@ -2,9 +2,26 @@ from textual.app import App, ComposeResult
 from textual.containers import VerticalScroll, HorizontalGroup, Center
 from textual.widgets import Header, Footer, Collapsible, Button, Static, Label
 
+from dotsaver import Software, Configuration
+
+BANNER = r"""
+ /$$$$$$$   /$$$$$$  /$$$$$$$$ /$$$$$$   /$$$$$$  /$$    /$$ /$$$$$$$$ /$$$$$$$ 
+| $$__  $$ /$$__  $$|__  $$__//$$__  $$ /$$__  $$| $$   | $$| $$_____/| $$__  $$
+| $$  \ $$| $$  \ $$   | $$  | $$  \__/| $$  \ $$| $$   | $$| $$      | $$  \ $$
+| $$  | $$| $$  | $$   | $$  |  $$$$$$ | $$$$$$$$|  $$ / $$/| $$$$$   | $$$$$$$/
+| $$  | $$| $$  | $$   | $$   \____  $$| $$__  $$ \  $$ $$/ | $$__/   | $$__  $$
+| $$  | $$| $$  | $$   | $$   /$$  \ $$| $$  | $$  \  $$$/  | $$      | $$  \ $$
+| $$$$$$$/|  $$$$$$/   | $$  |  $$$$$$/| $$  | $$   \  $/   | $$$$$$$$| $$  | $$
+|_______/  \______/    |__/   \______/ |__/  |__/    \_/    |________/|__/  |__/
+"""
+
 
 class SoftwareWidget(Static):
     __name: str
+    __software: Software
+
+    def __lt__(self, other):
+        return self.__name < other.__name
 
     def __init__(self, name):
         super().__init__()
@@ -19,34 +36,30 @@ class SoftwareWidget(Static):
 
 
 class DotsaverTui(App):
+    __config: Configuration
+    __software_list = [SoftwareWidget("yay"), SoftwareWidget("rg"), SoftwareWidget("nvim")]
     CSS_PATH = "dotsaver_tui.tcss"
     BINDINGS = [
         ("d", "toggle_dark", "Toggle dark mode"),
         ("a", "add_software", "Add"),
     ]
 
+    def __init__(self):
+        super().__init__()
+
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Footer()
         with Center():
-            yield Label(r"""
- /$$$$$$$   /$$$$$$  /$$$$$$$$ /$$$$$$   /$$$$$$  /$$    /$$ /$$$$$$$$ /$$$$$$$ 
-| $$__  $$ /$$__  $$|__  $$__//$$__  $$ /$$__  $$| $$   | $$| $$_____/| $$__  $$
-| $$  \ $$| $$  \ $$   | $$  | $$  \__/| $$  \ $$| $$   | $$| $$      | $$  \ $$
-| $$  | $$| $$  | $$   | $$  |  $$$$$$ | $$$$$$$$|  $$ / $$/| $$$$$   | $$$$$$$/
-| $$  | $$| $$  | $$   | $$   \____  $$| $$__  $$ \  $$ $$/ | $$__/   | $$__  $$
-| $$  | $$| $$  | $$   | $$   /$$  \ $$| $$  | $$  \  $$$/  | $$      | $$  \ $$
-| $$$$$$$/|  $$$$$$/   | $$  |  $$$$$$/| $$  | $$   \  $/   | $$$$$$$$| $$  | $$
-|_______/  \______/    |__/   \______/ |__/  |__/    \_/    |________/|__/  |__/
-                """)
+            yield Label(BANNER)
         with VerticalScroll(id="software_list"):
-            for i in sorted(["yay", "rg", "nvim"]):
-                yield SoftwareWidget(i)
+            for i in sorted(self.__software_list):
+                yield i
+        yield Footer()
 
     def action_add_software(self) -> None:
-        new_software = SoftwareWidget()
-        self.query_one("#software_list").mount(new_software)
-        new_software.scroll_visible()
+        new_software = SoftwareWidget("new")
+        self.__software_list.append(new_software)
+        self.refresh(recompose=True)
 
     def action_toggle_dark(self) -> None:
         self.theme = (
